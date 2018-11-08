f13b0c1b1c3ef24e3a28b3b67da24f33fdb07241
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-08 02:55:19 +0000

    automatically backed up by learn

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,14 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+
+  def new
+  end
+
+  def current_user
+
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
+end
